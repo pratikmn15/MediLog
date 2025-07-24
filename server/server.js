57d777fc8ee35b46b0854740e5039abf0debb228
Pratik Naik <naik.pratik004@hotmail.com>
2025-07-24 23:24:42 +0530

    fixed dashboard

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/user-details', userDetailsRoutes);
+app.use('/api/user-details', userDetailsRoutes);+
 app.use('/api/appointments', appointmentRoutes);
 
 // MongoDB Connect
